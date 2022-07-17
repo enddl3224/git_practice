4ae67dfa71c1d1b27de3fac2f90c0c6d112d05f5
enddl3224 <enddl3224@naver.com>
2022-07-17 17:08:56 +0900

    Add: views.py login 기능 추가

--- App1/urls.py
+++ App1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from App1.views import SignUpView
+from App1.views import LogInView, SignUpView
 
 urlpatterns=[
     path('/signup',SignUpView.as_view()),
+    path('/login', LogInView.as_view()),
 ]
 
